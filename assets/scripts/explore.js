d2928e884c19eacd3f82af8eb03ff7ff6fee4748
Peng Luo <p2luo@ucsd.edu>
2022-10-30 17:13:48 -0700

    Explore code finished

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -32,5 +32,35 @@ function init() {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
+
+  // read input text and option and speak
+  const input_text = document.querySelector("textarea");
+  const changing_image = document.querySelector("img");
+  const press_to_speak = document.querySelector("button");
+
+  press_to_speak.addEventListener("click", (event)=>{
+    const speak_unit = new SpeechSynthesisUtterance(input_text.value);
+
+    speak_unit.addEventListener("start", (event)=>{
+      changing_image.src = "assets/images/smiling-open.png";
+    })
+
+    speak_unit.addEventListener("end", (event)=>{
+      changing_image.src = "assets/images/smiling.png";
+    })    
+
+    const selected_option = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length; i++){
+      if (voices[i].name === selected_option){
+        speak_unit.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(speak_unit);
+  })
+
+
+
+
 }
 
